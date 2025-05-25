2a69917f79941e511e85ab4dc38509b0381c7940
Saurav Mittal <sauravagrawal2005@gmail.com>
2025-05-25 21:43:44 +0530

    Import Hook-UseState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+
+  const [color, setColor] = useState("olive")
 
   return<>
   <div className='w-full h-screen duration-200'>
