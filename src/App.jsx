348ecea0d84f59658f3325506540de433ee97f31
Saurav Mittal <sauravagrawal2005@gmail.com>
2025-05-25 21:44:55 +0530

    Add a Bydefault Color Using Hook

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [color, setColor] = useState("olive")
 
   return<>
-  <div className='w-full h-screen duration-200'>
+  <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
   <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
     <div className='flex flex-wrap gap-3 justify-center bg-white shadow-lg px-3  py-3 rounded-3xl'>
       <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg'>RED</button>
