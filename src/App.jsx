c5ef853996da21defa9d409ba7d5eab9faa2e9d6
Saurav Mittal <sauravagrawal2005@gmail.com>
2025-05-25 21:55:16 +0530

    Create a onclick event to change a Color

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,8 @@ function App() {
   return<>
   <div className='w-full h-screen duration-200' style={{backgroundColor: color}}>
   <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
-    <div className='flex flex-wrap gap-3 justify-center bg-black shadow-lg px-3  py-3 rounded-3xl'>
-      <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg'>RED</button>
+    <div className='flex flex-wrap gap-3 justify-center bg-white shadow-lg px-3  py-3 rounded-3xl'>
+      <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg' onClick={() => setColor("red")}>RED</button>
       
       <button className='outline-none px-4 py-1 rounded-full text-white shadow-lg'>Pink</button>
       
